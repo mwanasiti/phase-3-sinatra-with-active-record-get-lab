8f342f49aaf214d03d98b6ee5eb40b1c929c558b
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-13 09:31:08 +0300

    commit tree b13083a4c32b5af5594b122eb990eef405c0d38f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    all_bakeries = Bakery.all
+    all_bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    by_price = BakedGood.order(price: :desc)
+    by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.order('price DESC').first
+    most_expensive.to_json
+  end
 
 end
